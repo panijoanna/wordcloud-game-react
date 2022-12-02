3b8a65e20e68f7113e93a7f72cb4b2215325d93e
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-02 15:10:42 +0100

    add white color to the theme

--- src/theme.js
+++ src/theme.js
@@ -2,10 +2,11 @@ export const theme = {
   color: {
     mineshaft: "#1F1F1F",
     mercury: "#E6E6E6",
-    perano: "#BED8F3",
+    perano: "#90BEEB",
     green: "#8FCB92",
     deco: "#B4D68F",
     cinderella: "#FCD1CF",
     geraldine: "#F69B95",
+    white: "#FFFFFF",
   },
 };
