1a16ef7a5ae55ccc6a62e2718283ac3f25a26dcc
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-02 12:00:31 +0100

    add styles to the GlobalStyle

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,4 +1,21 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+html {
+    box-sizing: border-box;
+}
+
+*,
+::after,
+::before {
+    box-sizing: inherit;
+}
+
+body {
+    font-family: 'Karla', sans-serif;
+    font-weight: 200;
+    line-height: 1.5;
+    font-size: 16px;
+    margin: 0px auto;
+}
 `;
