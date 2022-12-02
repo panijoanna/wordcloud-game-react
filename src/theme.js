4c284ca630b55452cd2319c81933165e879eef78
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-02 11:13:24 +0100

    add colors to the theme

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,11 @@
 export const theme = {
-    color: {
-
-    }
+  color: {
+    mineshaft: "#1F1F1F",
+    mercury: "#E6E6E6",
+    perano: "#BED8F3",
+    green: "#8FCB92",
+    deco: "#B4D68F",
+    cinderella: "#FCD1CF",
+    geraldine: "#F69B95",
+  },
 };
