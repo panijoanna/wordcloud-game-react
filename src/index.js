d0f1342fa70b0fece7d9fa7545a8a1486424058c
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-02 11:52:02 +0100

    import theme and ThemeProvider in the index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GlobalStyle } from "./GlobalStyle";
+import { theme } from "./theme";
+import { ThemeProvider } from "styled-components";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <GlobalStyle/>
     <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
