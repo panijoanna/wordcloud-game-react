0a9719ec001211ae6aa7f0971cf74a45048794c2
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-02 15:13:46 +0100

    add styles to the FormButton

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -1,26 +1,27 @@
 import styled from "styled-components";
 
-export const StyledForm = styled.form`
-
-
-`;
+export const StyledForm = styled.form``;
 
 export const StyledInput = styled.input`
-border: 2px solid ${({ theme }) => theme.color.mercury};
-font-weight: 300;
-border-radius: 3px;
+  border: 2px solid ${({ theme }) => theme.color.mercury};
+  font-weight: 300;
+  border-radius: 2px;
 
-::placeholder {
+  ::placeholder {
     color: ${({ theme }) => theme.color.mineshaft};
-}
+  }
 `;
 
 export const LabelText = styled.label`
-font-family: 'Rubik', sans-serif;
-font-weight: 600;
-font-size: 20px;
+  font-family: "Rubik", sans-serif;
+  font-weight: 600;
+  font-size: 20px;
 `;
 
 export const FormButton = styled.button`
-
+  font-family: "Rubik", sans-serif;
+  border-radius: 4px;
+  background-color: ${({ theme }) => theme.color.white};
+  border: 2px solid ${({ theme }) => theme.color.perano};
+  color: ${({ theme }) => theme.color.perano};
 `;
