4270a782555afbf1f72d86c76810b0dd475b250b
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-02 13:38:55 +0100

    add form to the Form component

---
+++ src/Form/index.js
@@ -0,0 +1,21 @@
+
+const Form = () => {
+return (
+    <form>
+    <label>
+      Wordcloud game
+      <input
+             required
+             type="text"
+             name="nickname"
+             minlength="3"
+             maxlength="12"
+             placeholder="Enter your nickname here..."
+             />
+      <button>
+              play
+              </button>
+    </label>
+</form>
+)
+};
