bd80ecb1e1ddcbfdcdd5bc1397ec4db9f21d8eb1
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-02 14:57:47 +0100

    add styles to the StyledInput

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -5,6 +5,16 @@ export const StyledForm = styled.form`
 
 `;
 
+export const StyledInput = styled.input`
+border: 2px solid ${({ theme }) => theme.color.mercury};
+font-weight: 300;
+border-radius: 3px;
+
+::placeholder {
+    color: ${({ theme }) => theme.color.mineshaft};
+}
+`;
+
 export const LabelText = styled.label`
 font-family: 'Rubik', sans-serif;
 font-weight: 600;
