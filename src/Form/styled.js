dea0b05af720fe1ee40ce9922a7e512608b7065f
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-02 14:23:01 +0100

    export and add LabelText styles in the styled.js

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -1,1 +1,12 @@
 import styled from "styled-components";
+
+export const StyledForm = styled.form`
+
+
+`;
+
+export const LabelText = styled.label`
+font-family: 'Rubik', sans-serif;
+font-weight: 600;
+font-size: 20px;
+`;
