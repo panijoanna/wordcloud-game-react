27becba8b9cd75f205c2e1f80c3581b8c45f7693
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-03 09:59:23 +0100

    add padding and max-width to the button

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -27,6 +27,8 @@ export const LabelText = styled.label`
 export const FormButton = styled.button`
   font-family: "Rubik", sans-serif;
   border-radius: 4px;
+  padding: 7px;
+  max-width: 100px;
   background-color: ${({ theme }) => theme.color.white};
   border: 2px solid ${({ theme }) => theme.color.perano};
   color: ${({ theme }) => theme.color.perano};
