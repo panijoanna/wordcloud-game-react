078a85978efb34b3fed934407dddd2de57f945b7
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-03 10:16:57 +0100

    add align-items to the form

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const StyledForm = styled.form`
 display: flex;
 flex-direction: column;
+align-items: center;
 `;
 
 export const Container = styled.div`
