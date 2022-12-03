a4de20cf939764b458c975253763820c562c27d1
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-03 10:13:34 +0100

    add Container to the styled

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -3,6 +3,11 @@ import styled from "styled-components";
 export const StyledForm = styled.form`
 display: flex;
 flex-direction: column;
+`;
+
+export const Container = styled.div`
+display: flex;
+justify-content: center;
 margin: 40px;
 `;
 
