0d9262f4c155a8b085ef35d104de84af587a152f
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-06 11:06:50 +0100

    add styels to the CloudContainer

--- src/Form/Cloud/styled.js
+++ src/Form/Cloud/styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const CloudContainer = styled.div`
   border: 1px solid black;
-  width: 400px;
+  width: 500px;
+  height: 300px;
   border-radius: 4px;
 `;
