20be93ffe4ef7204de2b24778913077acd36516b
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-06 11:11:33 +0100

    add styles to the CloudButton

--- src/Form/Cloud/styled.js
+++ src/Form/Cloud/styled.js
@@ -25,5 +25,11 @@ font-size: 16px;
 `;
 
 export const CloudButton = styled.button`
-
+font-family: "Rubik", sans-serif;
+border-radius: 4px;
+padding: 5px 20px;
+margin-top: 25px;
+background-color:${({ theme }) => theme.color.white};
+border: 2px solid ${({ theme }) => theme.color.perano};
+color: ${({ theme }) => theme.color.perano};
 `;
