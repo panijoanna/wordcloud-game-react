857e1ec1b097ac406f01ab8b28bbe3a86beded41
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-06 11:12:03 +0100

    add hover to the CloudButton

--- src/Form/Cloud/styled.js
+++ src/Form/Cloud/styled.js
@@ -32,4 +32,8 @@ margin-top: 25px;
 background-color:${({ theme }) => theme.color.white};
 border: 2px solid ${({ theme }) => theme.color.perano};
 color: ${({ theme }) => theme.color.perano};
+
+&:hover {
+  filter: brightness(110%);
+}
 `;
