c3c4fd2a2e6a6f36a69aec93aee664d3c271e2fc
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-06 11:12:53 +0100

    add @media to the CloudButton

--- src/Form/Cloud/styled.js
+++ src/Form/Cloud/styled.js
@@ -12,28 +12,32 @@ export const CloudContainer = styled.div`
 `;
 
 export const MainContainer = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-margin: 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 80px;
 `;
 
 export const Heading = styled.h1`
-font-family: "Rubik", sans-serif;
-font-weight: 600;
-font-size: 16px;
+  font-family: "Rubik", sans-serif;
+  font-weight: 600;
+  font-size: 16px;
 `;
 
 export const CloudButton = styled.button`
-font-family: "Rubik", sans-serif;
-border-radius: 4px;
-padding: 5px 20px;
-margin-top: 25px;
-background-color:${({ theme }) => theme.color.white};
-border: 2px solid ${({ theme }) => theme.color.perano};
-color: ${({ theme }) => theme.color.perano};
+  font-family: "Rubik", sans-serif;
+  border-radius: 4px;
+  padding: 5px 20px;
+  margin-top: 25px;
+  background-color: ${({ theme }) => theme.color.white};
+  border: 2px solid ${({ theme }) => theme.color.perano};
+  color: ${({ theme }) => theme.color.perano};
 
-&:hover {
-  filter: brightness(110%);
-}
+  &:hover {
+    filter: brightness(110%);
+  }
+
+  @media (max-wdith: ${({ theme }) => theme.breakpoint.mobileDevice}px) {
+    width: 100%;
+  }
 `;
