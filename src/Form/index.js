03fbbcda85856e2b00d279ed5bba4da08d684510
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-07 11:31:57 +0100

    add onChange to the input

--- src/Form/index.js
+++ src/Form/index.js
@@ -32,6 +32,7 @@ const Form = () => {
           required
           autoComplete="off"
           value={nickName}
+          onChange={({ target }) => setNickName(target.value)}
           ref={inputRef}
           type="text"
           name="nickname"
