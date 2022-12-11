cef5bf8f5e4aeedd83fe480461f8d41c90a837b4
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-11 14:32:09 +0100

    return styles in the Result

--- src/Score/index.js
+++ src/Score/index.js
@@ -2,5 +2,11 @@ import { useState } from "react";
 import { ScoreContainer, ScoreParagraph } from "./styled";
 
 const Result = () => {
-const [score, setScore] = useState(0);
+  const [score, setScore] = useState(0);
+
+  return (
+    <ScoreContainer>
+      <ScoreParagraph>Congratulations,! Your score:</ScoreParagraph>
+    </ScoreContainer>
+  );
 };
