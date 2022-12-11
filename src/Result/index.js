ed0d2833750b14b41f8770c40d1521be75916d6e
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-11 19:59:29 +0100

    add StyledParagraph to the styled

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,13 +1,15 @@
 import { useState } from "react";
-import { ScoreContainer, StyledHeading } from "./styled";
+import { StyledParagraph, StyledHeading, ScoreContainer } from "./styled";
 
 const Result = () => {
   const [score, setScore] = useState(0);
 
   return (
     <ScoreContainer>
-      <StyledHeading>Congratulations, {localStorage.getItem("name")}!</StyledHeading> 
-      Your score:</ScoreParagraph>
+      <StyledHeading>
+        Congratulations, {localStorage.getItem("name")}!
+      </StyledHeading>
+      <StyledParagraph>Your score:</StyledParagraph>
     </ScoreContainer>
   );
 };
