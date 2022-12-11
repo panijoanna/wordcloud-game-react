5c45f1b3b21a24459062f783df05f4968a84c174
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-11 14:37:50 +0100

    import Result in the App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Form from "./Form";
 import Cloud from "./Cloud";
+import Result from "./Result";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/wordcloud-game-react" element={<Form />}></Route>
         <Route path="/cloud" element={<Cloud />}></Route>
+        <Route path="/score" element={<Result/>}></Route>
       </Routes>
     </>
   );
