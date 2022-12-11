325d5704153261e24c2eadfb74461ed37c90c111
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-11 14:27:41 +0100

    add styles to ScoreContainer

--- src/Score/styled.js
+++ src/Score/styled.js
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 
 export const ScoreParagraph = styled.p`
-font-family: "Rubik", sans-serif;
-font-size: 16px;
+  font-family: "Rubik", sans-serif;
+  font-size: 16px;
 `;
 
 export const ScoreContainer = styled.div`
-
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 400px;
 `;
