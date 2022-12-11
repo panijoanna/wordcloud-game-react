0f788bc886e9554c64431dcbdacbc1899047761f
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-11 14:16:23 +0100

    change width to max-width in the CloudContainer

--- src/Cloud/styled.js
+++ src/Cloud/styled.js
@@ -2,7 +2,7 @@ import styled, { css } from "styled-components";
 
 export const CloudContainer = styled.div`
   border: 1px solid black;
-  width: 500px;
+  max-width: 500px;
   height: 300px;
   border-radius: 4px;
 
